a8d8fb929ffabab980da355f23b117159f77a0e2
olivierbrt <o.berthiau@gmail.com>
2021-11-22 09:20:39 +0100

    New Front Server

--- annonces/src/app/app.component.ts
+++ annonces/src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'annonces';
-  version = '1.0';
 }
--- annonces/src/app/app.module.ts
+++ annonces/src/app/app.module.ts
@@ -1,28 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ItemDetailsComponent } from './item-details/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    ProfileSettingsComponent,
-    RegistrationComponent,
-    NotFoundComponent,
-    ItemDetailsComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
